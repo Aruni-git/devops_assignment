45d88f7411f45eb04feb14905c29d11a4376fadd
Aruni Gunapala <55123364+Aruni-git@users.noreply.github.com>
2024-01-10 08:46:32 +0000

    test cases removed

--- test_view_all.py
+++
@@ -1,9 +0,0 @@
-
-import requests
-
-def test_view_all_employees():
-    
-    url = "http://127.0.0.1:5000/allemployees"
-    
-    response = requests.get(url)
-    assert response.status_code == 200
